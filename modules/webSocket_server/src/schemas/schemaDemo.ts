efcc13310e43ab0fa4fac79dfc1d84c9f7dbf642
DoctorPok <pokdoctor@gmail.com>
2024-06-02 11:23:41 +0200

    Update schemaDemo.ts

--- modules/webSocket_server/src/schemas/schemaDemo.ts
+++ modules/webSocket_server/src/schemas/schemaDemo.ts
@@ -1,11 +1,11 @@
 import { Schema, model } from "mongoose";
 import { SchemaDemo } from "../types";
 
-const SchemaDemo = new Schema<SchemaDemo>({
+const schemas = new Schema<SchemaDemo>({
   key: { type: String, required: true, unique: true },
   createdAt: { type: Date, default: Date.now },
 });
 
-const SchemaDemoModel = model<SchemaDemo>("SchemaDemo", SchemaDemo);
+const SchemaDemoModel = model<SchemaDemo>("Schema", schemas);
 
 export default SchemaDemoModel;
